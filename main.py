2f24216e2b073d4cf7d5b792c76a086f260b90eb
Angelina <a.d.mironenko2003@gmail.com>
2023-04-02 17:52:49 +0300

    create base.html, start page. create picture link to another page.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def start_page():
+    return render_template("start.html", title="start")
+
+@app.route("/carousel")
+def carousel_page():
+    return render_template("carousel.html", title="carousel")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
+
 
